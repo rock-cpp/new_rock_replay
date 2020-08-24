d441c948c593d023b0c5a0e547aa0b85a11a075f
Dennis Hemker <dennis.hemker@dfki.de>
2020-08-24 10:43:20 +0200

    Added documentation

--- src/ReplayHandler.hpp
+++ src/ReplayHandler.hpp
@@ -8,67 +8,302 @@
 #include "LogTaskManager.hpp"
 
 
+/**
+ * @brief Class for handling replay of log samples.
+ * 
+ */
 class ReplayHandler
 {
     
 public:
+    /**
+     * @brief Constructor.
+     * 
+     */
     ReplayHandler() = default;
+    
+    /**
+     * @brief Destructor.
+     * 
+     */
     ~ReplayHandler();
     
+    /**
+     * @brief Stops replay. The current index is reset either to 0 or the min span.
+     */
     void stop();
+
+    /**
+     * @brief Starts replay. Execution is stopped after thre last sample was played.
+     * 
+     */
     void play();
+    
+    /**
+     * @brief Pauses replay. Execution can be resumed from the current index.
+     * 
+     */
     void pause();
     
+    /**
+     * @brief Moves the current index to the next sample. Only updates the metadata, actual replay 
+     * of sample is not performed. Can only be used when handler is paused/stopped.
+     */
     void next();
+
+    /**
+     * @brief Moves the current index to the previous sample. Only updates the metadata, actual replay 
+     * of sample is not performed. Can only be used when handler is paused/stopped.
+     */
     void previous();
+    
+    /**
+     * @brief Moves the current index to the given position. Can only be used it
+     * the handler is paused/stopped.
+     * 
+     * @param index: Index to set. Must be in [0, maxIndex].
+     */
     void setSampleIndex(uint64_t index);
     
+    /**
+     * @brief Sets the replay speed relatively.
+     * 
+     * @param speed: Speed to set. 1.00 means 100%.
+     */
     void setReplaySpeed(float speed);
+    
+    /**
+     * @brief Sets the minimum span. If replay has finished or is stopped, the current
+     * index is reset to the minimum span value.
+     * 
+     * @param minIdx: Minimum index.
+     */
     void setMinSpan(uint64_t minIdx);
+    
+    /**
+     * @brief Sets the maximum span. Repaly finishes after the maximum span is reached.
+     * 
+     * @param maxIdx: Maximum index.
+     */
     void setMaxSpan(uint64_t maxIdx);
     
+    /**
+     * @brief Activates/Deactivates replay for given port of task.
+     * 
+     * @param taskName: Name of task.
+     * @param portName: Name of port.
+     * @param on: True if replaying should be enabled, false otherwise.
+     */
     void activateReplayForPort(const std::string& taskName, const std::string& portName, bool on);
         
+    /**
+     * @brief Inits the replay handler for given logfiles.
+     * All replay parameters are updated and the current index is set to 0.
+     * 
+     * @param fileNames: List of file names.
+     */
     void init(const std::vector<std::string>& fileNames);
+    
+    /**
+     * @brief Deinits the replay handler. Closes all log tasks and allows
+     * new initing.
+     */
     void deinit();
+    
+    /**
+     * @brief Returns a list of pairs of task names with their corresponding port names.
+     * @return std::vector<std::pair<std::string, std::vector<std::string>>> Vector of task names with list of pairs.
+     */
     std::vector<std::pair<std::string, std::vector<std::string>>> getTaskNamesWithPorts();
     
+    /**
+     * @brief Returns the current sample timestamp.
+     * 
+     * @return const std::string Current timestamp.
+     */
     const std::string getCurTimeStamp() { return curMetadata.timeStamp.toString(); };
+    
+    /**
+     * @brief Returns the current sample port name.
+     * 
+     * @return const std::string Current port name.
+     */
     const std::string getCurSamplePortName() { return curMetadata.portName; };
+    
+    /**
+     * @brief Returns the current intex.
+     * 
+     * @return const uint Current index.
+     */
     const uint getCurIndex() { return curIndex; };
+    
+    /**
+     * @brief Returns the maximum possible index.
+     * 
+     * @return const size_t Maximum index.
+     */
     const size_t getMaxIndex() { return maxIdx; };
+    
+    /**
+     * @brief Returns the minimum span index.
+     * 
+     * @return const uint64_t Minimum span.
+     */
     const uint64_t getMinSpan() {return minSpan;};
+    
+    /**
+     * @brief Returns the maximum span index.
+     * 
+     * @return const uint64_t Maximum span.
+     */
     const uint64_t getMaxSpan() {return maxSpan;};
+    
+    /**
+     * @brief Returns the set relative target speed.
+     * 
+     * @return const double Commanded speed.
+     */
     const double getReplayFactor() { return targetSpeed; };
+    
+    /**
+     * @brief Returns the reached speed taking in account the target speed.
+     * 
+     * @return const double Current speed.
+     */
     const double getCurrentSpeed() { return currentSpeed; };
+    
+    /**
+     * @brief Returns whether playing has finished.
+     * 
+     * @return const bool True if playing is finished, false otherwise.
+     */
     const bool hasFinished() { return finished; };
+    
+    /**
+     * @brief Returns whether the replay handler is currently in playing mode.
+     * 
+     * @return const bool True if playing is active, false otherwise.
+     */
     const bool isPlaying() { return playing; };
-    const size_t getNumSamples() { return manager.getNumSamples(); };
     
 private:
+    /**
+     * @brief Starts the replay loop. Must be used in separate replay thread.
+     * 
+     */
     void replaySamples();
+    
+    /**
+     * @brief Calculates the time to sleep by using the timestamp of the next sample.
+     * 
+     */
     void calculateTimeToSleep();
+    
+    /**
+     * @brief Calculates the reached speed taking into account the target speed.
+     * 
+     */
     void calculateRelativeSpeed();
     
+    /**
+     * @brief Target speed.
+     * 
+     */
     float targetSpeed;
+    
+    /**
+     * @brief Current speed.
+     * 
+     */
     float currentSpeed;
+    
+    /**
+     * @brief Current sample metadata. Metadata that is displayed is replayed next.
+     * 
+     */
     LogTaskManager::SampleMetadata curMetadata;
+    
+    /**
+     * @brief Current index.
+     * 
+     */
     uint64_t curIndex;
+    
+    /**
+     * @brief Minimum span index.
+     * 
+     */
     uint64_t minSpan;
+    
+    /**
+     * @brief Maximum span index.
+     * 
+     */
     uint64_t maxSpan;
+    
+    /**
+     * @brief Maximum possible index.
+     * 
+     */
     uint64_t maxIdx;
+    
+    /**
+     * @brief Indicator if replaying is finished.
+     * 
+     */
     bool finished;
+    
+    /**
+     * @brief Indicator if replay thread should run.
+     * 
+     */
     bool running;
+    
+    /**
+     * @brief Indicator if replaying is active.
+     * 
+     */
+    bool playing;
+    
+    /**
+     * @brief Mutex to lock replay variables related to replay. 
+     * 
+     */
     std::mutex playMutex;
+    
+    /**
+     * @brief Condition to wake up thread if replay starts or stops.
+     * 
+     */
     std::condition_variable playCondition;
+    
+    /**
+     * @brief Replay thread.
+     * 
+     */
     std::thread replayThread;
     
+    /**
+     * @brief Timestamp of previous sample.
+     * 
+     */
     base::Time previousSampleTime;
-    base::Time timeBeforeSleep;
-    int64_t timeToSleep;
     
-    bool playing;
-        
+    /**
+     * @brief Tracks the system time before going to sleep; used to calculate reached speed.
+     * 
+     */
+    base::Time timeBeforeSleep;
+    
+    /**
+     * @brief Time to sleep before replaying next sample.
+     * 
+     */
+    int64_t timeToSleep;
 
+    /**
+     * @brief Log task manager.
+     * 
+     */
     LogTaskManager manager;
 };
