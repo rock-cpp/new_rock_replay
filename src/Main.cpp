7f05b0bf9c76d0dcc546e73f08f31ee26b1399b3
Dennis Hemker <dennis.hemker@dfki.de>
2020-09-15 14:27:04 +0200

    Secure commandline arg parsing

--- src/Main.cpp
+++ src/Main.cpp
@@ -5,16 +5,17 @@
 
 std::string prefix;
 std::vector<std::string> fileArgs;
+std::vector<std::string> fileNames;
 
-void parseArguments(int argc, char* argv[])
+bool parseArguments(int argc, char* argv[])
 {
     using namespace boost::program_options;
     
-    options_description desc("Usage: rock-replay2 {logfile|*}.log\nAvailable options");
+    options_description desc("Usage: rock-replay2 {logfile|*}.log or folder\nAvailable options");
     desc.add_options()
         ("help", "show this message")
         ("prefix", value<std::string>(), "add prefix to all tasks")
-        ("log-files", value<std::vector<std::string>>(), "log files");
+        ("log-files", value<std::vector<std::string>>(&fileArgs), "log files");
 
     positional_options_description p;
     p.add("log-files", -1);
@@ -23,9 +24,11 @@ void parseArguments(int argc, char* argv[])
     store(command_line_parser(argc, argv).options(desc).positional(p).run(), vm);
     notify(vm);    
 
-    if(vm.count("help"))
+    fileNames = LogFileHelper::parseFileNames(fileArgs);
+    if(vm.count("help") || !vm.count("log-files") || fileNames.empty())
     {
         std::cout << desc << std::endl;
+        return false;
     }
     
     if(vm.count("prefix"))
@@ -33,22 +36,22 @@ void parseArguments(int argc, char* argv[])
         prefix = vm["prefix"].as<std::string>();
     }
     
-    if(vm.count("log-files"))
-    {
-        fileArgs = vm["log-files"].as<std::vector<std::string>>();
-    }
+    return true;
 }
     
 int main(int argc, char *argv[])
 {
-    parseArguments(argc, argv);
+    if(parseArguments(argc, argv))
+    {
+        QApplication a(argc, argv);
+        ReplayGui gui;
+        
+        gui.initReplayHandler(fileNames, prefix);
+        gui.updateTaskView();
+        
+        gui.show();    
+        return a.exec();
+    }
     
-    QApplication a(argc, argv);
-    ReplayGui gui;
-    
-    gui.initReplayHandler(LogFileHelper::parseFileNames(fileArgs), prefix);
-    gui.updateTaskView();
-    
-    gui.show();    
-    return a.exec();
+    return 0;
 }
