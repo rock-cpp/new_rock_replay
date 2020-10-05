0d47428aefbe635c32a726737bc2a1cf285a707d
Dennis Hemker <dennis.hemker@dfki.de>
2020-10-05 15:58:28 +0200

    Add more unit tests

--- src/LogFileHelper.cpp
+++ src/LogFileHelper.cpp
@@ -36,11 +36,12 @@ std::pair<std::string, std::string> LogFileHelper::splitStreamName(const std::st
 {
     std::string taskName = streamName;
 
-    size_t pos = taskName.find('/');
-    if(taskName.size() && pos != std::string::npos)
-    {
-        taskName = taskName.substr(pos + 1, taskName.size());
-    }
+    //FIXME: Why to remove /?
+//     size_t pos = taskName.find('/');
+//     if(taskName.size() && pos != std::string::npos)
+//     {
+//         taskName = taskName.substr(pos + 1, taskName.size());
+//     }
 
     size_t portSplitToken = taskName.find_last_of('.');
     return {taskName.substr(0, portSplitToken), taskName.substr(portSplitToken + 1, taskName.size())};
--- test/LogTaskManagerTest.cpp
+++ test/LogTaskManagerTest.cpp
@@ -1,11 +1,39 @@
-#include <boost/test/unit_test.hpp>
 #include "LogTaskManager.hpp"
+
 #include "LogFileHelper.hpp"
 
+#include <boost/test/unit_test.hpp>
+#include <set>
+
 const auto fileNames = LogFileHelper::parseFileNames({"../logs/trajectory_follower_Logger.0.log"});
+LogTaskManager manager;
 
 BOOST_AUTO_TEST_CASE(TestSpawnTrajectoryFollower)
 {
-    LogTaskManager manager;
+    const std::set<std::pair<std::string, std::string>> portsWithTypes = {
+        {"follower_data", "/trajectory_follower/FollowerData"}, {"motion_command", "/base/commands/Motion2D"}, {"state", "int"}};
+
     manager.init(fileNames, "");
+
+    auto tasks = manager.getTaskCollection();
+    auto trajectoryFollowerTask = tasks.at("trajectory_follower");
+    auto orderedPortsWithType = std::set<std::pair<std::string, std::string>>(trajectoryFollowerTask.begin(), trajectoryFollowerTask.end());
+
+    BOOST_TEST(manager.getNumSamples() == 849);
+    BOOST_TEST(tasks.size() == 1);
+    BOOST_TEST(orderedPortsWithType == portsWithTypes);
 }
+
+BOOST_AUTO_TEST_CASE(TestTaskMetaData)
+{
+    auto metadata = manager.setIndex(250);
+
+    BOOST_TEST(metadata.valid);
+    BOOST_TEST(metadata.portName == "trajectory_follower.motion_command");
+}
+
+// BOOST_AUTO_TEST_CASE(TestReplay)
+// {
+//     orocos_cpp::OrocosCpp orocos;
+//     auto task = orocos.getTaskContext("trajectory_follower");
+// }
